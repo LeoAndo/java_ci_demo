9e48fd8d2147278e78c2b5024698abbccfe8963b
ando <leo81@outlook.jp>
2023-06-13 18:03:56 +0900

    update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,11 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         int a = 20;
+        Foo();
         System.out.println("Hello world!");
+    }
+
+    private static void Foo() {
+
     }
 }
