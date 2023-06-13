b63d9ff71965dd90eceeee5d023433d5b35e3375
ando <leo81@outlook.jp>
2023-06-13 18:25:31 +0900

    レビュー指摘反映

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,11 +3,11 @@ package org.example;
 public class Main {
   public static void main(String[] args) {
     int a = 20;
-    Boo();
+    foo();
     System.out.println("Hello world!");
   }
 
-  private static void Boo() {
+  private static void foo() {
 
   }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -11,61 +11,31 @@ import java.io.PrintStream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MainTest {
-    private ByteArrayOutputStream byteArrayOutputStream;
+  private ByteArrayOutputStream byteArrayOutputStream;
 
-    @BeforeEach
-    void setUp() {
-        byteArrayOutputStream = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(new BufferedOutputStream(byteArrayOutputStream)));
-    }
+  @BeforeEach
+  void setUp() {
+    byteArrayOutputStream = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(new BufferedOutputStream(byteArrayOutputStream)));
+  }
 
-    @AfterEach
-    void tearDown() {
-        // System.setOut(System.out);
-    }
+  @AfterEach
+  void tearDown() {
+    // System.setOut(System.out);
+  }
 
-    @Test
-    @DisplayName("mainメソッドを実行")
-    public void main() {
-        // main()メソッドを実行
-        Main.main(new String[]{});
+  @Test
+  @DisplayName("mainメソッドを実行")
+  public void main() {
+    // main()メソッドを実行
+    Main.main(new String[]{});
 
-        // 標準出力の内容を取得
-        System.out.flush();
-        final String actual = byteArrayOutputStream.toString();
+    // 標準出力の内容を取得
+    System.out.flush();
+    final String actual = byteArrayOutputStream.toString();
 
-        // 期待値を設定
-        final String expected = "Hello world!" + System.lineSeparator();
-        assertEquals(expected, actual, "Hello world!となるように修正して");
-    }
-
-    @Test
-    @DisplayName("mainメソッドを実行2")
-    public void main2() {
-        // main()メソッドを実行
-        Main.main(new String[]{});
-
-        // 標準出力の内容を取得
-        System.out.flush();
-        final String actual = byteArrayOutputStream.toString();
-
-        // 期待値を設定
-        final String expected = "Hello world!2" + System.lineSeparator();
-        assertEquals(expected, actual, "Hello world!となるように修正して");
-    }
-
-    @Test
-    @DisplayName("mainメソッドを実行3")
-    public void main3() {
-        // main()メソッドを実行
-        Main.main(new String[]{});
-
-        // 標準出力の内容を取得
-        System.out.flush();
-        final String actual = byteArrayOutputStream.toString();
-
-        // 期待値を設定
-        final String expected = "Hello world!3" + System.lineSeparator();
-        assertEquals(expected, actual, "Hello world!となるように修正して");
-    }
+    // 期待値を設定
+    final String expected = "Hello world!" + System.lineSeparator();
+    assertEquals(expected, actual, "Hello world!となるように修正して");
+  }
 }
