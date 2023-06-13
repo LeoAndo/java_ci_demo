fe4bb4e8d5fc24f17b9328dfb972fbac93877fd9
ando <leo81@outlook.jp>
2023-06-13 18:47:59 +0900

    JUnit/checkStyleどちらも成功パターン

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,9 @@
 package org.example;
 
+/**
+ * Leo Ando.
+ * テストクラス
+ */
 public class Main {
   /**
    * メインの処理.
@@ -7,5 +11,6 @@ public class Main {
    * @param args コマンドライン引数
    */
   public static void main(String[] args) {
+    System.out.println("Hello world!");
   }
 }
